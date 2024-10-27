057358cc178b156a1da86717817c2aa8d3277fbf
AlexanderKorepin <1987kerykery@mail.ru>
2024-10-27 20:11:15 +0200

    hW_2

---
+++ hw_2/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import CommentsList from './components/commentsList';
+import "./components/commentsList.css";
+
+function App() {
+  return (
+    <div>
+      <CommentsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ hw_2/src/components/commentsList.js
@@ -0,0 +1,30 @@
+
+import {useState} from "react";
+
+function CommentsList() {
+
+    const [comments, setComments] = useState([
+        { id: 1, text: "Это первый комментарий" },
+        { id: 2, text: "Это второй комментарий" },
+        { id: 3, text: "Это третий комментарий" }
+        ]);
+        const deleteComments = (id) => {
+            setComments(comments.filter((comment) => comment.id !== id));
+        };
+    
+        return (
+            <div>
+                <h2>Коментарии:</h2>
+                <ul class="markers">
+                    {comments.map((comment) => (
+                        <li>
+                            {comment.text} <button class="btn" onClick={() => deleteComments(comment.id)}>Удаалить комментарий</button>
+                            
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+export default CommentsList;
